e899cc1db47b10b8838efe05a3a05c799a655570
bbaker <brian.baker@ritebytes.com>
2023-10-09 07:46:44 -0700

    Favorites can now be toggled and state persists.

--- Landmarks/Views/Landmarks/LandmarkDetail.swift
+++ Landmarks/Views/Landmarks/LandmarkDetail.swift
@@ -8,8 +8,18 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    @Environment(ModelData.self) var modelData
+                 
     var landmark: Landmark
+                 
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id})!
+    }
+    
+    
     var body: some View {
+        @Bindable var modelData = modelData
+        
         ScrollView {
             
             MapView(coordinate: landmark.locationCoordinate)
@@ -20,8 +30,11 @@ struct LandmarkDetail: View {
                 .padding(.bottom, -130)
             
             VStack(alignment: .leading) {
-                Text(landmark.name)
-                    .font(.title)
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
                 HStack {
                     Text(landmark.park)
                       
@@ -47,5 +60,7 @@ struct LandmarkDetail: View {
 }
 
 #Preview {
-    LandmarkDetail(landmark: ModelData().landmarks[0])
+    let modelData = ModelData()
+    return LandmarkDetail(landmark: modelData.landmarks[0])
+        .environment(modelData)
 }
