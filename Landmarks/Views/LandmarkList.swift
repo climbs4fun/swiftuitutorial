3a6fe147bf8fb10e41a7dbc71cefa1ef68d652e2
bbaker <brian.baker@ritebytes.com>
2023-10-09 07:24:26 -0700

    updates

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -8,13 +8,39 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @Environment(ModelData.self) var modelData
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
+    
     var body: some View {
-        List(landmarks, id: \.id){ landmark in
-            LandmarkRow(landmark: landmark)
+        NavigationSplitView{
+                List {
+                    Toggle(isOn: $showFavoritesOnly, label: {
+                        Text("Favorites Only")
+                    })
+                    ForEach(filteredLandmarks) {landmark in
+                        NavigationLink{
+                            LandmarkDetail(landmark: landmark)
+                        } label: {
+                            LandmarkRow(landmark: landmark)
+                        }
+                    }
+            }
+            .navigationTitle("Landmarks")
+        } detail: {
+            Text("Select a Landmark")
+        
         }
     }
 }
 
 #Preview {
     LandmarkList()
+        .environment(ModelData())
 }
