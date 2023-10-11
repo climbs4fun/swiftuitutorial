b42b51126735ed9e0e3ab898c2fbd4fed3e2c4cb
bbaker <brian.baker@ritebytes.com>
2023-10-10 20:16:08 -0700

    Added Featured to CategoryHome and tweak interface

--- Landmarks/Views/Categories/CategoryHome.swift
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -14,9 +14,19 @@ struct CategoryHome: View {
         NavigationView {
             //Text("Test")
            List {
+               modelData.features[0].image
+                   .resizable()
+                   .scaledToFill()
+                   .frame(height: 200)
+                   .clipped()
+                   .listRowInsets(EdgeInsets())
+               
+               
                 ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
-                    Text(key)
+                    CategoryRow(categoryName: key, items: modelData.categories[key]!
+                    )
                 }
+                .listRowInsets(EdgeInsets())
             }
             .navigationTitle("Featured")
         }
