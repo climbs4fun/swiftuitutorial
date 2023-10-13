5e5cfa2b400bf8743725ea90d40bd8bc6bf0d5ce
bbaker <brian.baker@ritebytes.com>
2023-10-12 17:46:12 -0700

    UIKit stuff

--- Landmarks/Views/PageView/PageViewController.swift
+++ Landmarks/Views/PageView/PageViewController.swift
@@ -5,25 +5,23 @@
 //  Created by Brian Baker on 10/12/23.
 //
 
-import Foundation
 import SwiftUI
 import UIKit
 
 struct PageViewController<Page: View>: UIViewControllerRepresentable {
     var pages: [Page]
 
-    func makeUIViewController(context: Context) -> some UIViewController {
-        let pageViewController = UIPageViewController(
-            transitionStyle: .scroll,
-            navigationOrientation: .horizontal)
+    func makeUIViewController(context: Context) -> UIPageViewController {
+            let pageViewController = UIPageViewController(
+                transitionStyle: .scroll,
+                navigationOrientation: .horizontal)
         
         return pageViewController
     }
     
-    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
-        pageViewController.setViewController(
-            [UIHostingController(rootView: pages[0])], direction: .forward, animated: true)
-                                             
-    }
+    func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
+            pageViewController.setViewControllers(
+                [UIHostingController(rootView: pages[0])], direction: .forward, animated: true)
+        }
 
 }
